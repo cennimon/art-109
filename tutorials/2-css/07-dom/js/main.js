12e293ccc846524cb7bb0a53e10af6b5db484227
cennimon <enandai99@gmail.com>
2026-02-27 15:46:12 -0800

    tut 9 w error

---
+++ tutorials/2-css/07-dom/js/main.js
@@ -0,0 +1,41 @@
+// console.log('hello')
+
+// select html elements
+const header = document.querySelector('#header');
+const changeHeaderButton = document.querySelector('#change-header-button');
+const changeThemeButton = document.querySelector('#change-theme-button');
+const img1 = document.querySelector('#img1');
+const img2 = document.querySelector('#img2');
+const img3 = document.querySelector('#img3');
+
+// change header w button click
+changeHeaderButton.addEventListener('click', () => {
+    header.innerHTML = 'thin mint';
+})
+
+//toggle color theme
+
+//create function for changing button text
+function changeButtonText() {
+    if (document.boby.classList.contains('dark')) {
+        changeThemeButton.textContent = 'switch to light theme';
+    } else {
+        changeThemeButton.textContent = 'switch to dark theme';
+    }
+}
+
+//click event on button
+changeThemeButton.addEventListener('click', () => {
+    //add/remove dark class to body
+    document.body.classList.toggle('dark');
+    changeButtonText();
+})
+
+//toggle image visibility
+img1.addEventListener('click', () => {
+    img2.classList.remove('hidden');
+})
+
+img2.addEventListener('click', () => {
+    img3.classList.remove('hidden');
+})
