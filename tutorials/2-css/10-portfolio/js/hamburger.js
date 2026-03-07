1507bf592930cdcb03a6aef84d2c619bc8733ab7
cennimon <enandai99@gmail.com>
2026-03-07 00:32:11 -0800

    portfolio

--- tutorials/2-css/10-portfolio/js/hamburger.js
+++
@@ -1,8 +0,0 @@
-// console.log("hello");
-
-const hamburger = document.querySelector('.hamburger');
-const menu = document.querySelector('.nav-menu');
-
-hamburger.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
