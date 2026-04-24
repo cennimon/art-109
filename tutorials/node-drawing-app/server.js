1a6be591720ec90d3d195226b6babca486a92adb
cennimon <enandai99@gmail.com>
2026-04-24 16:55:35 -0700

    part of node tut

---
+++ tutorials/node-drawing-app/server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+
+var app = express();
+var server = app.listen(3000);
+
+app.use(express.static("public"));
+
+console.log("hello world");
+
+var socket = require("socket.io");
