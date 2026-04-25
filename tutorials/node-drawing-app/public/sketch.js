25fb45236fa63b05ee0cd671c83cab767ed73860
cennimon <enandai99@gmail.com>
2026-04-24 22:44:24 -0700

    node tut

--- tutorials/node-drawing-app/public/sketch.js
+++ tutorials/node-drawing-app/public/sketch.js
@@ -1,15 +1,34 @@
 var socket;
 
 function setup() {
-createCanvas(200, 200);
- background(51);
+    createCanvas(600, 600);
+    background(51);
 
-socket = io.connect('http://localhost:3000');
+    socket = io.connect('http://localhost:3000');
+    socket.on("mouse", newDrawing);
+}
+
+function newDrawing(data) {
+    noStroke();
+    fill(255, 0, 100);
+    ellipse(data.x, data.y, 30, 30);
+}
+
+function mouseDragged() {
+    console.log("sending: " + mouseX + "," + mouseY);
+
+    var data = {
+        x: mouseX,
+        y: mouseY
+    }
+    socket.emit('mouse', data);
+
+    noStroke();
+    fill(255);
+    ellipse(mouseX, mouseY, 30, 30);
 }
 
 function draw() {
-    noStroke();
-    fill(255);
-    ellipse(mouseX, mouseY, 60, 60);
+
 }
 
--- tutorials/node-drawing-app/server.js
+++ tutorials/node-drawing-app/server.js
@@ -15,4 +15,11 @@ io.sockets.on("connection", newConnection);
 
 function newConnection(socket) {
     console.log("new connection: " + socket.id);
+    socket.on("mouse", mouseMsg);
+
+    function mouseMsg(data) {
+        socket.broadcast.emit("mouse", data);
+        console.log(data);
+
+    }
 }
